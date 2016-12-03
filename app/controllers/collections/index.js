46d0deb2b0106a20620e87678459cf7e0077fb12
Conor Linehan <conorlinehan9@gmail.com>
2016-12-03 21:52:32 +0000

    setup collection ui

---
+++ app/controllers/collections/index.js
@@ -0,0 +1,10 @@
+import Ember from 'ember';
+
+export default Ember.Controller.extend({
+
+  actions: {
+    addCollection() {
+      this.get('store').createRecord('collection').save();
+    }
+  }
+});
