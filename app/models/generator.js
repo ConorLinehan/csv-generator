2de7dd76f4a7ce21fe6c4e96f6965184e58cb36b
Conor Linehan <conorlinehan9@gmail.com>
2016-12-03 13:32:01 +0000

    setup collection model

--- app/models/generator.js
+++ app/models/generator.js
@@ -1,14 +1,22 @@
-import Model from 'ember-data/model';
-import attr from 'ember-data/attr';
+import DS from 'ember-data';
 import Ember from 'ember';
 
 const {
   computed,
 } = Ember;
 
+const {
+  Model,
+  belongsTo,
+  attr
+} = DS;
+
 export default Model.extend({
   name: attr('string'),
   fakerPath: attr('string', {defaultValue: 'name.findName'}),
+
+  collection: belongsTo('collection', { async: true, autoSave: true }),
+
   generatorCategory: computed('fakerPath', {
     get() {
       return this.get('fakerPath').split('.')[0];
--- tests/unit/models/generator-test.js
+++ tests/unit/models/generator-test.js
@@ -2,7 +2,7 @@ import { moduleForModel, test } from 'ember-qunit';
 
 moduleForModel('generator', 'Unit | Model | generator', {
   // Specify the other units that are required for this test.
-  needs: []
+  needs: ['model:collection']
 });
 
 test('it exists', function(assert) {
