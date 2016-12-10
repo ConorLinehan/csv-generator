a1193d4b003103e22194e955d2af23898f6762cd
Conor Linehan <conorlinehan9@gmail.com>
2016-12-10 14:10:04 +0000

    adds progress message to worker

--- tests/unit/workers/gen-data-test.js
+++ tests/unit/workers/gen-data-test.js
@@ -9,10 +9,13 @@ test('it generates fake data', function(assert) {
   let worker = new Worker('/assets/gen-data.js');
 
   worker.addEventListener('message', ({ data }) =>{
-    assert.ok((typeof data === 'string'), 'returns unparsed data');
-    let parsedData = Papa.parse(data).data;
-    assert.equal(parsedData.length, 10, 'generated ten rows');
-    done();
+    if (data.result) {
+      let result = data.result;
+      assert.ok((typeof result === 'string'), 'returns unparsed data');
+      let parsedData = Papa.parse(result).data;
+      assert.equal(parsedData.length, 10, 'generated ten rows');
+      done();
+    }
   });
 
   worker.addEventListener('error', (e) =>{
@@ -23,6 +26,37 @@ test('it generates fake data', function(assert) {
 
   let data = {
     rows: 10,
+    paths: [ [['random'], ['number']], [['name'], ['firstName']] ]
+  };
+
+  worker.postMessage(data);
+});
+
+test('it posts back progress', function(assert) {
+  let done = assert.async();
+  let worker = new Worker('/assets/gen-data.js');
+
+  let messages = [];
+
+  worker.addEventListener('message', ({ data }) =>{
+    if (data.result) {
+      assert.deepEqual(
+        messages,
+        [0, 20, 40, 60, 80],
+        'sends progress messages'
+      );
+      done();
+    } else {
+      messages.push(data.progress);
+    }
+  });
+
+  worker.addEventListener('error', () =>{
+    done();
+  });
+
+  let data = {
+    rows: 500,
     paths: [ [['random'], ['number']], [['name'], ['firstName']] ]
   };
 
