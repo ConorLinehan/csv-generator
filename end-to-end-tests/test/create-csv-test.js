73152544935f341fe760f1f48f6f0f9083eb066f
Conor Linehan <conorlinehan9@gmail.com>
2016-12-23 23:37:40 +0000

    [FEATURE] end to end tests & deployments (#13)

---
+++ end-to-end-tests/test/create-csv-test.js
@@ -0,0 +1,72 @@
+"use strict";
+
+var Nightmare = require('nightmare');
+require('nightmare-inline-download')(Nightmare);
+var nightmare = Nightmare({
+    switches: {
+      'ignore-certificate-errors': true
+    },
+});
+var assert = require('chai').assert;
+var mocha = require('mocha');
+var coMocha = require('co-mocha');
+var csv = require('fast-csv');
+var RSVP = require('rsvp');
+
+coMocha(mocha);
+
+const URL = 'https://csv-generator.staging.pagefrontapp.com/';
+const TEN_SECONDS = 1e4;
+const WORKER_TRIGGER_NUMBER = 700;
+
+/**
+ * Returns Promise with csv data
+ * @param  {String} path
+ * @return {Promise}
+ */
+function parseCSV(path) {
+  return new RSVP.Promise(function(resolve, reject) {
+    let result = [];
+
+    csv
+    .fromPath(path)
+    .on('data', function(data) {
+      result.push(data);
+    })
+    .on('end', function() {
+      resolve(result);
+    });
+  });
+}
+
+
+describe('Downloads CSV File', function() {
+  this.timeout(TEN_SECONDS);
+
+  before(function* (){
+    return yield nightmare.goto(URL);
+  });
+
+  it('can create csv [NON Worker]', function*() {
+    yield nightmare
+    .click('.collection-item')
+    .click('button.generate')
+    .download('./csv_dump/non_worker.csv');
+
+    let result = yield parseCSV('./csv_dump/non_worker.csv');
+    assert.lengthOf(result, 101, 'creates csv');
+  });
+
+  it('can create csv [WORKER]', function*() {
+    yield nightmare
+    .insert('.row-count input', '')
+    .insert('.row-count input', WORKER_TRIGGER_NUMBER)
+    .click('button.generate')
+    .download('./csv_dump/worker.csv');
+
+    let result = yield parseCSV('./csv_dump/worker.csv');
+
+    assert.lengthOf(result, 701, 'creates csv');
+  });
+
+});
---
+++ travis_scripts/end-to-end-test.sh
@@ -0,0 +1,7 @@
+set -ev
+
+ember deploy staging
+cd end-to-end-tests
+mkdir csv_dump
+npm install
+npm test
