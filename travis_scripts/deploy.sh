131fba707d7c39705101f690a5ac5f823ad543ab
Conor Linehan <conorlinehan9@gmail.com>
2016-12-23 22:23:04 +0000

    set up travis scripts

---
+++ travis_scripts/deploy.sh
@@ -0,0 +1,11 @@
+set -e
+
+if [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
+  exit 0
+fi
+
+if [ "$TRAVIS_BRANCH" == "master" ]; then
+  ember deploy staging
+elif [ "$TRAVIS_BRANCH" == "release" ]; then
+  ember deploy production
+fi
---
+++ travis_scripts/end-to-end-test.sh
@@ -0,0 +1,5 @@
+ember deploy staging
+cd end-to-end-tests
+npm install
+npm test
+cd $TRAVIS_BUILD_DIR
